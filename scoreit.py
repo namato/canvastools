7bbff434fb74e75f1191f433eeacbdf99999f3df
namato <amaton@gmail.com>
2019-03-16 00:38:23 -0700

    handle late assignments

--- scoreit.py
+++ scoreit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/home/namato/anaconda3/bin/python
 
 # XXX needed because of
 # https://github.com/Anorov/PySocks/issues/119
@@ -13,6 +13,7 @@ import sys
 from pprint import pprint
 from canvasgrader import CanvasGrader
 import time
+import datetime
 
 # add your info here
 API_URL = "https://mycampus.instructure.com"
@@ -21,7 +22,7 @@ API_KEY = ""
 # Initialize a new Canvas object
 canvas = Canvas(API_URL, API_KEY)
 
-def doprompt(name, override):
+def doprompt_student(name, override):
     if (override == True):
         print('Matched student %s' % name)
         return True
@@ -33,11 +34,29 @@ def doprompt(name, override):
             return False
         else:
             print('Please enter \'y\' or \'n\'')
-            return doprompt()
+            return doprompt_student()
     except Exception as e:
         print('Please enter \'y\' or \'n\'')
         print(e)
-        return doprompt()
+        return doprompt_student()
+
+def doprompt_late(pts, sc, override):
+    if (override == True):
+        return True
+    a = str(input("Submission was %d days late, subtract %d points (new score: %d)?" %
+              (pts, pts, sc - pts)))
+    try:
+        if a[0] == 'y':
+            return True
+        elif a[0] == 'n':
+            return False
+        else:
+            print('Please enter \'y\' or \'n\'')
+            return doprompt_late()
+    except Exception as e:
+        print('Please enter \'y\' or \'n\'')
+        print(e)
+        return doprompt_late()
 
 def dejunk(s):
     t = str.maketrans({key: None for key in string.punctuation})
@@ -71,13 +90,26 @@ def main():
     choice = None
     for i in ids:
         if (args.name in i[0].split(',')[0]):
-            if (doprompt(i[0], args.y)):
+            if (doprompt_student(i[0], args.y)):
                 choice = i[1]
                 break
     if (choice == None):
         print('No student chosen or no matches, exiting')
         sys.exit(0)
     print('proceeding with id %d' % choice)
+
+    a = cr.get_assignment(args.assign_id)
+    s = a.get_submission(choice)
+    due = a.due_at_date
+    sa = datetime.datetime.strptime(s.submitted_at, "%Y-%m-%dT%H:%M:%S%z")
+    l = sa - due
+    dlate = l.days
+    if (l.days == 0 and l.seconds > 0):
+        dlate = 1
+    if (dlate > 0):
+        if (doprompt_late(dlate, args.points, args.y)):
+            args.points -= dlate
+            print('Proceeding with late deduction of %d points (to %d)' % (dlate, args.points))
 
     grades_for_canvas = {}
     k1 = 'grade_data[{student_id}][posted_grade]'.format(student_id=choice)
